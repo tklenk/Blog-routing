e328db2a6a4f84d14481f02d9a49273016c4e852
Tanya Kl <tatiana.klenk@gmail.com>
2020-01-17 20:52:52 +0100

    Using HISTORY PROPS to redirect (replace)

--- containers/Blog/NewPost/NewPost.js
+++ containers/Blog/NewPost/NewPost.js
@@ -25,7 +25,10 @@ class NewPost extends Component {
         axios.post('/posts', data)
             .then(response => {
                 console.log(response);
-                this.setState({ submitted: true })
+                this.props.history.push('/posts')
+                // this.props.history.replace('/posts')
+                
+                // this.setState({ submitted: true })
             });
     }
 
